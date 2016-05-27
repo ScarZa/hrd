82300ab76312319ecc3ac3f0163110f2d1532554
ScarZ <ScarZ@ScarZ>
2016-05-27 10:25:07 +0700

    privet calendra 26/05/59
    Conflicts:
    	README.md

---
+++ add_privatet_calendra.php
@@ -0,0 +1,32 @@
+<?php @session_start(); ?>
+<?php include 'connection/connect.php'; ?>
+<?php if($_REQUEST['method']!='back'){
+if (empty($_SESSION[user])) {
+    echo "<meta http-equiv='refresh' content='0;url=index.php'/>";
+    exit();
+}}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
+        <title>ระบบข้อมูลบุคคลากรโรงพยาบาล</title>
+        <LINK REL="SHORTCUT ICON" HREF="images/logo.png">
+        <!-- Bootstrap core CSS -->
+        <link href="option/css/bootstrap.css" rel="stylesheet">
+        <!--<link href="option/css2/templatemo_style.css" rel="stylesheet">-->
+        <!-- Add custom CSS here -->
+        <link href="option/css/sb-admin.css" rel="stylesheet">
+        <link rel="stylesheet" href="option/font-awesome/css/font-awesome.min.css">
+        <!-- Page Specific CSS -->
+        <link rel="stylesheet" href="option/css/morris-0.4.3.min.css">
+        <link rel="stylesheet" href="option/css/stylelist.css">
+        <script src="option/js/excellentexport.js"></script>
+        </head>
+    <body>
+
+<?php include 'footer.php'; ?>
